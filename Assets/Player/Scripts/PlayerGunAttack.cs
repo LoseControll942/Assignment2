88de620917b36ee032e66e5aab05349d5c05bcd5
LoseControll942 <b.chester90@yahoo.com>
2026-05-06 23:29:24 +0100

    Added reload animations and refined reload and ammo system

--- Assets/Player/Scripts/PlayerGunAttack.cs
+++ Assets/Player/Scripts/PlayerGunAttack.cs
@@ -21,12 +21,30 @@ public class PlayerGunAttack : MonoBehaviour
     [SerializeField]
     int magAmmo = 8;
 
+    [SerializeField] 
+    GameObject ammoUI;
+
+    [SerializeField]
+    GameObject magUI;
+
+    [SerializeField]
+    GameObject gunArms;
+
+    [SerializeField]
+    float reloadTime = 4f;
+
     [SerializeField]
     LayerMask layerMask;
+
+    bool canShoot;
 
     [Header("UI")]
     public TextMeshProUGUI ammoText;
     public TextMeshProUGUI magAmmoText;
+
+    [Header("Animation")]
+    public Animation animation;
+    public AnimationClip ReloadAnim;
    
     PlayerInput playerInput;
     InputAction attackAction;
@@ -35,10 +53,17 @@ public class PlayerGunAttack : MonoBehaviour
     void Start()
     {
         this.enabled = false;
+        ammoUI.SetActive(false);
+        magUI.SetActive(false);
+        gunArms.SetActive(false);
     }
     void OnEnable()
     {
         playerInput = GetComponent<PlayerInput>();
+        canShoot = true;
+        gunArms.SetActive(true);
+        ammoUI.SetActive(true);
+        magUI.SetActive(true);
         var map = playerInput.currentActionMap;
         attackAction = map.FindAction("Attack", true);
         reloadAction = map.FindAction("Reload", true);
@@ -58,16 +83,21 @@ public class PlayerGunAttack : MonoBehaviour
 
         }
 
-        if (reloadAction.triggered && ammo != 8)
+        if (reloadAction.triggered && ammo != 8 && mag !=0)
         {
-            Reload();
+            canShoot = false;
+            Debug.Log("Reloading...");
+            animation.Play(ReloadAnim.name);
+            Invoke("Reload", reloadTime);
         }
+
 
     }
     void FireWeapon()
     {
 
-
+        if (canShoot == true)
+        {
             cameraTransform = Camera.main.transform;
             Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
             RaycastHit raycastHit;
@@ -88,10 +118,12 @@ public class PlayerGunAttack : MonoBehaviour
             {
                 Debug.Log("NO RAYCAST");
             }
+        }
     }
 
     void Reload()
     {
+        canShoot = false;
         if (mag > 0)
         {
             mag--;
@@ -101,5 +133,7 @@ public class PlayerGunAttack : MonoBehaviour
 
         magAmmoText.text = mag.ToString();
         ammoText.text = ammo + "/" + magAmmo;
+        canShoot = true;
+        Debug.Log("Reloaded!");
     }
 }
